7b1f2e45aaf34558c8c4c5c546e007ae498ec465
xu <jiangxu525@gmail.com>
2020-05-30 10:31:39 -0400

    step 3

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
         Button myBtn = findViewById(R.id.myBtn);
 
